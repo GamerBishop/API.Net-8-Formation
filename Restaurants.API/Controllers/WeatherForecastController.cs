46b64dfe1c132438f2a2984c39d7a9b72e86a7c6
Gamerbishop <hugo.levk@gmail.com>
2024-09-08 19:18:25 +0200

    Système d'injection de dépendance intégré

--- Restaurants.API/Controllers/WeatherForecastController.cs
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -6,27 +6,22 @@ namespace Restaurants.API.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly WeatherForecastService p_WeatherForecastService;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherForecastService weatherForecastService)
     {
         _logger = logger;
+        p_WeatherForecastService = weatherForecastService;
+
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return p_WeatherForecastService.Get();
+        }
     }
 
-    [HttpGet]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
 }
