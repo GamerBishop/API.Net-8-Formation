648dd58879d5495e16427440a0e286285052e29e
Gamerbishop <hugo.levk@gmail.com>
2024-09-10 16:09:15 +0200

    Status Code

--- Restaurants.API/Controllers/WeatherForecastController.cs
+++ Restaurants.API/Controllers/WeatherForecastController.cs
@@ -21,22 +21,31 @@ public class WeatherForecastController : ControllerBase
     public IEnumerable<WeatherForecast> Get()
     {
         _logger.LogInformation("Getting weather forecast");
-        return p_WeatherForecastService.Get();
+        return p_WeatherForecastService.Get( 5, -20, 55 );
     }
 
     [HttpGet]
     [Route("{take}/ExampleGet")]
-    public IEnumerable<WeatherForecast> Get([FromQuery] int max, [FromRoute] int take)
+    public IActionResult Get([FromQuery] int max, [FromRoute] int take)
     {
         _logger.LogInformation("Getting weather forecast");
-        return p_WeatherForecastService.Get();
+
+        //if (take > 35000)
+        //{
+        //    Response.StatusCode = 400;
+        //}
+
+        var result = p_WeatherForecastService.Get(5, -20, 55);
+
+        //return StatusCode(400, result); => ObjectResult as return value
+        return BadRequest(result);
     }
 
     [HttpGet("CurrentDay")]
     public WeatherForecast GetCurrent()
     {
-        _logger.LogInformation("Getting weather forecast");
-        return p_WeatherForecastService.Get().First();
+        _logger.LogInformation("Gettin55 -20 55g weather forecast");
+        return p_WeatherForecastService.Get(1, -20, 55).First();
     }
 
     [HttpPost]
