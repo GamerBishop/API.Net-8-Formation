6b9f351732423f5ea686908a2ed834871b4eaf93
HugoLevk <hugo.levk@gmail.com>
2024-09-27 23:35:15 +0200

    Services de Présentation

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -6,42 +6,16 @@ using Serilog.Events;
 using Restaurants.API.Middlewares;
 using Restaurants.Domain.Entities;
 using Microsoft.OpenApi.Models;
+using Restaurants.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference{ Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
-            },
-            []
-        }
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<TimeLoggingMiddleware>();
+builder.AddPresentation();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
 
 var app = builder.Build();
 
