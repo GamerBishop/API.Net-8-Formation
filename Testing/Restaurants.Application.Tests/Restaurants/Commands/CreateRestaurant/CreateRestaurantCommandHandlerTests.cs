1c18c2dba45f465a4b9f757deb1c963d638d350a
HugoLevk <hugo.leveque@web-atrio.com>
2024-10-24 14:16:40 +0200

    CommandHandler tests

---
+++ Testing/Restaurants.Application.Tests/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Restaurants.Application.Users;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+
+namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant.Tests;
+
+public class CreateRestaurantCommandHandlerTests
+{
+    [Fact()]
+    public async Task Handle_ForValidCommand_ReturnCreatedRestaurantId()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<CreateRestaurantCommandHandler>>();
+        var mapperMock = new Mock<IMapper>();
+
+        var restaurant = new Restaurant();
+
+
+        mapperMock
+            .Setup(x => x.Map<Restaurant>(It.IsAny<CreateRestaurantCommand>()))
+            .Returns(restaurant);
+
+        var _guid = Guid.NewGuid();
+
+        var restaurantRepositoryMock = new Mock<IRestaurantRepository>();
+        restaurantRepositoryMock
+            .Setup(x => x.CreateAsync(It.IsAny<Restaurant>()))
+            .ReturnsAsync(_guid);
+
+        var userContextMock = new Mock<IUserContext>();
+        var CurrentUser = new CurrentUser("owner-id", "test@test.com", [], null, null);
+        userContextMock.Setup(x => x.GetCurrentUser()).Returns(CurrentUser);
+
+        var commandHandler = new CreateRestaurantCommandHandler(loggerMock.Object, mapperMock.Object, restaurantRepositoryMock.Object, userContextMock.Object);
+
+        // Act
+        var result = await commandHandler.Handle(new CreateRestaurantCommand(), CancellationToken.None);
+
+        // Assert
+        result.Should().Be(_guid);
+        restaurant.OwnerId.Should().Be(CurrentUser.Id);
+        restaurantRepositoryMock.Verify(x => x.CreateAsync(restaurant), Times.Once);
+    }
+}
